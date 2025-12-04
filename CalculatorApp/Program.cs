f233ceebe49df61b557348c3a5db342551868464
sapigulla <sapi@usr.tech>
2025-12-04 15:43:06 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Добро пожаловать в калькулятор!");
+        Console.WriteLine("Введите первое число:");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Результат сложения: {a+b}");
+    }
+}
