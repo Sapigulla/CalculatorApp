c63767f642c34943e30d4543fa0826c1b2c02cde
sapigulla <sapi@usr.tech>
2025-12-04 21:59:47 +0300

    Убрал Line

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -3,13 +3,13 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Добро пожаловать в калькулятор!");
-        Console.WriteLine("Введите первое число:");
+        Console.Write("Введите первое число:");
         double a = double.Parse(Console.ReadLine()!);
 
-        Console.WriteLine("Введите второе число:");
+        Console.Write("Введите второе число:");
         double b = double.Parse(Console.ReadLine()!);
 
-        Console.WriteLine("Выберите операцию (+ или *):");
+        Console.Write("Выберите операцию (+ или *):");
         char op = Console.ReadLine()![0];
 
         double result = op == '+' ? a + b : a * b;
