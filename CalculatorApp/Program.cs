abad8f8d491b8e5ccfebd1f61ee4b3ef17b04c37
sapigulla <sapi@usr.tech>
2025-12-04 16:39:26 +0300

    Добавлен ! чтобы программа не ругалась

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -4,10 +4,10 @@
     {
         Console.WriteLine("Добро пожаловать в калькулятор!");
         Console.WriteLine("Введите первое число:");
-        double a = double.Parse(Console.ReadLine());
+        double a = double.Parse(Console.ReadLine()!);
 
         Console.WriteLine("Введите второе число:");
-        double b = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine()!);
 
         Console.WriteLine($"Результат сложения: {a+b}");
     }
